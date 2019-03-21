a57df4ee8633d410ae0f1c25ea8dde6c120653d7
Joffily Ferreira <joffily@outlook.com>
2019-03-21 15:49:37 -0300

    Torna a inicialização de pastas mais dinâmica
    Este commit tem como premissa tornar a criação de pastas mais dinâmica.
    O INITIAL_PATH passa a receber as pastas definidas em variáveis de ambiente.

--- documentstore_migracao/config.py
+++ documentstore_migracao/config.py
@@ -13,18 +13,55 @@ _default = dict(
     LOGGER_PATH=os.path.join(BASE_PATH, ""),
 )
 
+
+def get(config: str):
+    """Recupera configurações do sistema, caso a configuração não
+    esteja definida como uma variável de ambiente deve-se retornar a
+    configuração padrão.
+    """
+    return os.environ.get(config, _default.get(config, ""))
+
 INITIAL_PATH = [
-    _default["LOGGER_PATH"],
-    _default["SOURCE_PATH"],
-    _default["SUCCESS_PROCESSING_PATH"],
-    _default["CONVERSION_PATH"],
-    _default["GENERATOR_PATH"],
+    get("LOGGER_PATH"),
+    get("SOURCE_PATH"),
+    get("SUCCESS_PROCESSING_PATH"),
+    get("CONVERSION_PATH"),
+    get("GENERATOR_PATH"),
 ]
 
 DOC_TYPE_XML = """<!DOCTYPE article PUBLIC "-//NLM//DTD JATS (Z39.96) Journal Publishing DTD v1.1 20151215//EN" "JATS-journalpublishing1.dtd">"""
 
 os.environ["XML_CATALOG_FILES"] = XML_CATALOG
 
-
-def get(config):
-    return os.environ.get(config, _default.get(config, ""))
+ALLOYED_TAGS_TO_P = [
+    "abstract",
+    "ack",
+    "annotation",
+    "app",
+    "app-group",
+    "author-comment",
+    "author-notes",
+    "bio",
+    "body",
+    "boxed-text",
+    "caption",
+    "def",
+    "disp-quote",
+    "fig",
+    "fn",
+    "glossary",
+    "list-item",
+    "note",
+    "notes",
+    "open-access",
+    "ref-list",
+    "sec",
+    "speech",
+    "statement",
+    "supplementary-material",
+    "support-description",
+    "table-wrap-foot",
+    "td",
+    "th",
+    "trans-abstract",
+]
