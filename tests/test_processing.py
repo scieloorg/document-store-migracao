03aa685829fdbf6b76bede33b616b249d8adb3a7
Joffily Ferreira <joffily@outlook.com>
2019-03-26 15:03:54 -0300

    Adiciona função para ler json de períodicos

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -96,3 +96,21 @@ class TestProcessingReading(unittest.TestCase):
                 if "Test Error - READING" in log_message:
                     has_message = True
             self.assertTrue(has_message)
+
+class TestReadingJournals(unittest.TestCase):
+    def setUp(self):
+        self.journals_json_path = os.path.join(
+            SAMPLES_PATH, "base-isis-sample", "title"
+        )
+
+    def test_should_load_file_successfull(self):
+        with utils.environ(SOURCE_PATH=self.journals_json_path):
+            data = reading.read_journals_from_json("title.json")
+
+            self.assertTrue(type(data), list)
+            self.assertEqual(
+                data[0].get("v140")[0]["_"],
+                "Colégio Brasileiro de Cirurgia Digestiva - CBCD",
+            )
+
+            self.assertEqual(len(data), 3)
