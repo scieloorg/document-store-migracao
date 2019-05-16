a2a6898d0c296dcab5099717e0860f9fe33809d2
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-05-16 15:24:18 -0300

    Aplica o registro de estágios para a extração de artigos
    Este commit habilita o registro de estágios para a extração de artigos, tal
    modificação torna possível parar a extração em um determinado estágio e continuá-la
    posteriormente sem prejuizo ao processo.

--- documentstore_migracao/processing/extracted.py
+++ documentstore_migracao/processing/extracted.py
@@ -11,10 +11,21 @@ logger = logging.getLogger(__name__)
 
 
 def extract_all_data(list_documents_pids: List[str]):
+    """Extrai documentos XML a partir de uma lista de PIDS
+    de entrada"""
+
+    pids_to_extract, pids_extracteds, stage_path = files.fetch_stages_info(
+        list_documents_pids, __name__
+    )
 
     logger.info("Iniciando extração dos Documentos")
     count = 0
-    for documents_pid in tqdm(list_documents_pids):
+
+    for documents_pid in tqdm(
+        iterable=pids_to_extract,
+        initial=len(pids_extracteds),
+        total=len(list_documents_pids),
+    ):
         documents_pid = documents_pid.strip()
 
         logger.debug("\t coletando dados do Documento '%s'", documents_pid)
@@ -27,5 +38,6 @@ def extract_all_data(list_documents_pids: List[str]):
             )
             logger.debug("\t Salvando arquivo '%s'", file_path)
             files.write_file(file_path, xml_article)
+            files.register_latest_stage(stage_path, documents_pid)
 
     logger.info("\t Total de %s artigos", count)
