0d2da372e9c57427d87b08e21ffac75b54ff6420
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-05-17 15:33:39 -0300

    Suaviza interrupções via teclado durante a extração

--- documentstore_migracao/processing/extracted.py
+++ documentstore_migracao/processing/extracted.py
@@ -21,23 +21,26 @@ def extract_all_data(list_documents_pids: List[str]):
     logger.info("Iniciando extração dos Documentos")
     count = 0
 
-    for documents_pid in tqdm(
-        iterable=pids_to_extract,
-        initial=len(pids_extracteds),
-        total=len(list_documents_pids),
-    ):
-        documents_pid = documents_pid.strip()
+    try:
+        for documents_pid in tqdm(
+            iterable=pids_to_extract,
+            initial=len(pids_extracteds),
+            total=len(list_documents_pids),
+        ):
+            documents_pid = documents_pid.strip()
 
-        logger.debug("\t coletando dados do Documento '%s'", documents_pid)
-        xml_article = article.ext_article_txt(documents_pid)
-        if xml_article:
-            count += 1
+            logger.debug("\t coletando dados do Documento '%s'", documents_pid)
+            xml_article = article.ext_article_txt(documents_pid)
+            if xml_article:
+                count += 1
 
-            file_path = os.path.join(
-                config.get("SOURCE_PATH"), "%s.xml" % documents_pid
-            )
-            logger.debug("\t Salvando arquivo '%s'", file_path)
-            files.write_file(file_path, xml_article)
-            files.register_latest_stage(stage_path, documents_pid)
+                file_path = os.path.join(
+                    config.get("SOURCE_PATH"), "%s.xml" % documents_pid
+                )
+                logger.debug("\t Salvando arquivo '%s'", file_path)
+                files.write_file(file_path, xml_article)
+                files.register_latest_stage(stage_path, documents_pid)
+    except KeyboardInterrupt:
+        ...
 
     logger.info("\t Total de %s artigos", count)
