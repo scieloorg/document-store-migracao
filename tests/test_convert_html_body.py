e199853c960fa6b9dc900cd59416d3bdf21ed9da
Roberta Takenaka <roberta.takenaka@scielo.org>
2019-07-23 15:31:55 -0300

    Renomeia FixElementAPipe  para AddNameAndIdToElementAPipe

--- tests/test_convert_html_body.py
+++ tests/test_convert_html_body.py
@@ -1333,7 +1333,7 @@ class TestConversionToFig(unittest.TestCase):
         html_pl = HTML2SPSPipeline(pid="S1234-56782018000100011")
         pl = ConvertElementsWhichHaveIdPipeline(html_pl)
 
-        text, xml = pl.FixElementAPipe(html_pl).transform((text, xml))
+        text, xml = pl.AddNameAndIdToElementAPipe(html_pl).transform((text, xml))
         text, xml = pl.DocumentPipe(html_pl).transform((text, xml))
         _xml = etree.tostring(xml)
         self.assertIn(
@@ -1478,7 +1478,7 @@ class TestConvertElementsWhichHaveIdPipeline(unittest.TestCase):
         text = """<root><a name="_ftnref19" href="#_ftn2" id="_ftnref19"><sup>1</sup></a></root>"""
         expected = b"""<root><a name="_ftnref19" id="_ftnref19"/><a href="#_ftn2"><sup>1</sup></a></root>"""
         xml = etree.fromstring(text)
-        text, xml = self.pl.FixElementAPipe(self.html_pl).transform((text, xml))
+        text, xml = self.pl.AddNameAndIdToElementAPipe(self.html_pl).transform((text, xml))
         self.assertEqual(etree.tostring(xml), expected)
 
     def test_pipe_asterisk_in_a_href(self):
