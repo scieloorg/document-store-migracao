86e72ee5e37ef4bb6ac8895792de0ccd433f9b47
Joffily Ferreira <joffily.ferreira@scielo.org>
2019-11-13 15:06:22 -0300

    Permite o armazenamento em memória do extração MST
    Este commit altera a função de extração de arquivos MST para possibilitar
    a captura do resultado. A função passa a permitir que o resultado não seja
    salvo em disco.
    
    Esta modificação torna o uso da função mais conveniênte por outras partes
    do código.
    
    Também remove a supressão das exceções ao tentar ler arquivos.

--- documentstore_migracao/utils/extract_isis.py
+++ documentstore_migracao/utils/extract_isis.py
@@ -20,28 +20,34 @@ def create_output_dir(path):
         os.makedirs(output_dir)
 
 
-def run(path: str, output_file: str):
+def run(path: str, output_file: str = "") -> dict:
     """Roda um subprocesso com o isis2json de target para extrair dados
     de uma base ISIS em formato MST. O resultado da extração
     é armazenado em formato JSON em arquivo determinado pelo
-    parâmetro output_file.
+    parâmetro ``output_file``.
+
+    Se o parâmetro ``output_file`` não for utilizado o resultado da extração
+    não será gravado em disco.
     """
 
-    command = "java -cp %s org.python.util.jython %s -t 3 -p 'v' -o %s %s" % (
+    if not os.path.exists(path):
+        raise FileNotFoundError("File '%s' does not exist.")
+
+    if output_file:
+        output_command = "-o %s" % output_file
+    else:
+        output_command = ""
+
+    command = "java -cp %s org.python.util.jython %s -t 3 -p 'v' %s %s" % (
         config.get("CLASSPATH"),
         ISIS2JSON_PATH,
-        output_file,
+        output_command,
         path,
     )
 
-    try:
-        logger.debug("Extracting database file: %s" % path)
-        subprocess.run(
-            shlex.split(command),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            check=True,
-        )
-        logger.debug("Writing extracted result as JSON file in: %s" % output_file)
-    except Exception as exc:
-        raise exceptions.ExtractError(str(exc)) from None
+    return subprocess.run(
+        shlex.split(command),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        check=True,
+    )
