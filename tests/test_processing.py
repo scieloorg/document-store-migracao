b3a11e73615364765d635b3be1af1b8f3864bab6
Patricia Morimoto <patricia.morimoto@scielo.org>
2020-02-14 15:52:48 -0300

    Altera/corrige testes unitários de conversão

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import tempfile
+import shutil
 from lxml import etree
 from unittest.mock import patch, ANY, call, Mock, MagicMock
 
@@ -38,62 +40,25 @@ class TestProcessingExtracted(unittest.TestCase):
 
 
 class TestProcessingConversion(unittest.TestCase):
-    @patch("documentstore_migracao.processing.conversion.SPS_Package")
-    @patch("documentstore_migracao.processing.conversion.xml")
-    def test_convert_article_xml_sets_sps_and_dtd_versions(
-        self, mk_utils_xml, MockSPS_Package
-    ):
-        mk_obj_xml = MagicMock()
-        mk_obj_xmltree = MagicMock()
-        mk_obj_xmltree.getroot.return_value = mk_obj_xml
-        mk_utils_xml.loadToXML.return_value = mk_obj_xmltree
-        conversion.convert_article_xml(
-            os.path.join(SAMPLES_PATH, "S0036-36341997000100001.xml")
-        )
-        mk_obj_xml.set.assert_has_calls(
-            [call("specific-use", "sps-1.9"), call("dtd-version", "1.1")]
-        )
+    def setUp(self):
+        self.conversion_path = tempfile.mkdtemp()
 
-    @patch("documentstore_migracao.processing.conversion.SPS_Package")
-    @patch("documentstore_migracao.processing.conversion.xml")
-    def test_convert_article_xml_creates_sps_package_instance(
-        self, mk_utils_xml, MockSPS_Package
-    ):
-        mk_obj_xmltree = MagicMock()
-        mk_utils_xml.loadToXML.return_value = mk_obj_xmltree
-        conversion.convert_article_xml(
-            os.path.join(SAMPLES_PATH, "S0036-36341997000100001.xml")
+    def tearDown(self):
+        shutil.rmtree(self.conversion_path)
+
+    def test_convert_article_xml_saves_xml_obj_in_conversion_path(self):
+        file_xml_path = os.path.join(SAMPLES_PATH, "S0036-36341997000100001.xml")
+        with utils.environ(
+            SOURCE_PATH=SAMPLES_PATH, CONVERSION_PATH=self.conversion_path
+        ):
+            conversion.convert_article_xml(file_xml_path)
+
+        new_file_xml_path = os.path.join(
+            self.conversion_path, "S0036-36341997000100001.es.xml"
         )
-        MockSPS_Package.assert_called_once_with(mk_obj_xmltree)
+        self.assertTrue(os.path.exists(new_file_xml_path))
 
-    @patch("documentstore_migracao.processing.conversion.SPS_Package")
-    @patch("documentstore_migracao.processing.conversion.xml")
-    def test_convert_article_xml_calls_sps_package_transform_body(
-        self, mk_utils_xml, MockSPS_Package
-    ):
-        mk_xml_sps = MagicMock()
-        MockSPS_Package.return_value = mk_xml_sps
-        conversion.convert_article_xml(
-            os.path.join(SAMPLES_PATH, "S0036-36341997000100001.xml")
-        )
-        mk_xml_sps.transform_body.assert_called_once()
 
-    @patch("documentstore_migracao.processing.conversion.SPS_Package")
-    @patch("documentstore_migracao.processing.conversion.xml")
-    def test_convert_article_xml_calls_sps_package_transform_content(
-        self, mk_utils_xml, MockSPS_Package
-    ):
-        mk_xml_sps = MagicMock()
-        MockSPS_Package.return_value = mk_xml_sps
-        conversion.convert_article_xml(
-            os.path.join(SAMPLES_PATH, "S0036-36341997000100001.xml")
-        )
-        mk_xml_sps.transform_content.assert_called_once()
-
-    def test_convert_article_xml(self):
-
-        conversion.convert_article_xml(
-            os.path.join(SAMPLES_PATH, "S0036-36341997000100001.xml")
         )
 
     @patch("documentstore_migracao.processing.conversion.convert_article_xml")
