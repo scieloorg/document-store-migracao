97c7bccf6ec3d428946858d0cfe7699a18bcd3ff
jamilatta <atta.jamil@gmail.com>
2020-08-24 16:28:24 -0300

    Divide o arquivo .csv de dados suplementares do ano

---
+++ script/gen_supplemet_csv_by_year.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+START=2002
+END=2020
+FILE_PATH=$1
+YEAR_FILE_NAME_SUFIX="_supplement_native_xml"
+
+for i in $(seq $START $END);
+do
+    echo "Ano: "$i;
+    filename=$i$YEAR_FILE_NAME_SUFIX'.csv';
+    awk -F ',' '$4 ~ /^ *'$i'/ {print}' $FILE_PATH > $filename;
+    echo "Gerado arquivo: "$filename" para ano: "$i
+done;
